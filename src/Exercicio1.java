c96a9d9ac65415b376ae249e864e992ff6770556
Claudio Vicente Rodrigues Junior <claudio.vrj20@gmail.com>
2025-08-30 16:00:25 -0300

    first commit

---
+++ src/Exercicio1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo numero: ");
+        int numero2 = scanner.nextInt();
+
+        int resultado = numero1 * numero2;
+
+        System.out.println("O resultado é igual: " + resultado);
+    }
+}
